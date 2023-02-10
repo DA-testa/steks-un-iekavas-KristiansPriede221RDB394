22885ec1ff520318882a32ef980af8e79ff28a1d
KristiansPriede221RDB394 <124790245+KristiansPriede221RDB394@users.noreply.github.com>
2023-02-10 19:49:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,24 +14,16 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-             opening_brackets_stack.append(i)
             pass
 
         if next in ")]}":
             # Process closing bracket, write your code here
-           rigth _index= rigth.index(i)
             pass
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    if  opening_brackets_stack and ( opening_brackets_stack[-1]==rigth[rigth_index])
-     opening_brackets_stack.pop()
-        else:
-        return rigth_index
-    if len( opening_brackets_stack)==0
-    return "Success"
     # Printing answer, write your code here
     
     
